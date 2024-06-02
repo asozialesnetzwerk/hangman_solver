eea2388d490944bf99068c7015aebf9a698eaa91
Joshix <joshix.dev@mailbox.org>
2024-06-02 14:00:00 +0000

    Revert "replace_string_in_place refactor"
    This reverts commit 3fc59374c8d2533ee7fc838a41c252ec4e675ca5.

--- build.rs
+++ build.rs
@@ -200,14 +200,12 @@ fn replace_string_in_place<S, RR, RC>(
     RC: Fn(char) -> Option<S>,
     RR: Fn(&mut String, Range<usize>, S),
 {
-    let replacements: Vec<_> = string
+    while let Some((idx, ch, repl)) = string
         .char_indices()
         .rev()
-        .filter_map(|(idx, ch)| replace_char(ch).map(|repl| (idx..idx + ch.len_utf8(), repl)))
-        .collect();
-    for (range, repl) in replacements
+        .find_map(|(idx, ch)| replace_char(ch).map(|repl| (idx, ch, repl)))
     {
-        replace_range(string, range, repl);
+        replace_range(string, idx..idx + ch.len_utf8(), repl);
     }
 }
 
