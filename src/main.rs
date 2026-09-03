25c7f9c445901d7a976729277b8fbf7fc177cfb0
Joshix <joshix.dev@mailbox.org>
2026-09-03 20:00:00 +0000

    ignore missing const for fn

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,7 @@ use crate::language::Language;
 use crate::solver::InfallibleCharCollection as _;
 use crate::solver::solve;
 
+#[cfg_attr(not(feature = "terminal_size"), expect(clippy::missing_const_for_fn))]
 fn get_terminal_width() -> usize {
     #[cfg(feature = "terminal_size")]
     if let Some((Width(w), _)) = terminal_size() {
